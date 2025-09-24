eda5050911f062b9427504443b50812718922587
voyager-postman <mobappssolutions160@gmail.com>
2025-09-24 09:48:17 +0530

    24-09-2025

--- src/Pages/AccountVerified.jsx
+++ src/Pages/AccountVerified.jsx
@@ -9,20 +9,21 @@ const AccountVerified = () => {
   const queryParams = new URLSearchParams(location.search);
   const reason = queryParams.get("reason");
   const email = queryParams.get("email");
-    const role = queryParams.get("role");
-
+  const role = queryParams.get("role");
 
   useEffect(() => {
     console.log("Reason:", reason);
     console.log("Email:", email);
   }, [reason, email]);
   const handleContinue = () => {
-    // Navigate to home page
-    if(role==="JobSeeker")
-    navigate("/login");else{
-   navigate("/login")
+    // Navigate to login based on role
+    if (role === "JobSeeker") {
+      navigate("/login");
+    } else {
+      navigate("/employer-login");
     }
   };
+
   return (
     <div className="verified-container">
       <div className="verified-card">
@@ -34,8 +35,8 @@ const AccountVerified = () => {
           Congratulations, <span className="username">{email}</span>
         </p>
         <p className="message">
-           We have verified your application and confirmed your status in your
-        profile. You can now log in and start using your account.
+          We have verified your application and confirmed your status in your
+          profile. You can now log in and start using your account.
         </p>
 
         <div className="personal-info-btn">
